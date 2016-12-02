6ad1726714b156345bf6d04ed36414ce34c4d897
Ben Neigher <bneigher@gmail.com>
2016-12-02 14:53:50 -0800

    updated with PR changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-### v1.2.0
+### (feature/client-support)
 - Ported to work both on the client side as well as server side
 
 ### v1.1.2
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
-var derequire = require('gulp-derequire');
-var watchify = require('watchify');
-var babelify = require('babelify');
-var notify = require('gulp-notify');
+'use strict';
+
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
+const derequire = require('gulp-derequire');
+const watchify = require('watchify');
+const babelify = require('babelify');
+const notify = require('gulp-notify');
 
 
 function compile (watch) {
-  var bundler = browserify({
+  let bundler = browserify({
     // Required watchify args
     cache: {}, packageCache: {},
     // Specify the entry point of your app
     entries: ['./lib/ptolemy.js'],
-    // Add file extentions to make optional in your requires
-    extensions: ['.js'],
     // Enable source maps!
     debug: true,
     // Allows this to be included from as bundled library
@@ -24,16 +24,15 @@ function compile (watch) {
 
   bundler
   .transform(babelify.configure({
-    presets: ['latest'],
-    extensions: ['.js']
+    presets: ['latest']
   }))
   .transform('debowerify');
 
-  var bundle = function () {
+  let bundle = function () {
     console.log('Bundling ->');
     return bundler.bundle()
       .on('error', function() {
-        var args = Array.prototype.slice.call(arguments);
+        let args = Array.prototype.slice.call(arguments);
         // Send error to notification center with gulp-notify
         notify.onError({
           title: 'Compile Error',
